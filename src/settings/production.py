141a39df42f0534d21c780dd6288ca03e976872d
shaxruxtleubaev <tleubaevshaxrux@outlook.com>
2023-09-02 20:08:40 +0500

    moving to mysql

--- src/settings/production.py
+++ src/settings/production.py
@@ -2,20 +2,20 @@ from .base import *
 
 DEBUG = False
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'farruxb1_mysql',
-        'USER': 'farruxb1_user',
-        'PASSWORD': 'gg1357908642',
-        'HOST': 'localhost',
-        'POST': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'farruxb1_mysql',
+#         'USER': 'farruxb1_user',
+#         'PASSWORD': 'gg1357908642',
+#         'HOST': 'localhost',
+#         'POST': '3306'
+#     }
+# }
